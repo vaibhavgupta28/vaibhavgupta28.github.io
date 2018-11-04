2000a41885e10ebbaa87785af8e92d45f5b1c184
Vai Gupta <vaibhavgupta28@gmail.com>
2018-11-04 14:10:17 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,162 @@
+// Questions
+var q1 = [
+	'What are you working on?',
+	'How does your product work in more detail?',
+	'Who in your team does what?',
+	'How did your team meet?      ',
+	'Who would use your product?',
+	'How many users do you have?',
+	'How are you meeting customers?',
+	'How did your team meet?',
+	'What else have you created together?',
+	'Who are your competitors?',
+	'What is your burn rate?',
+	'Who else are in your team?',
+	'Who would you hire or how would you add to your team? Who would be your next hire?',
+	'What are the key things about your field that outsiders don’t understand?',
+	'Why will you succeed over others?'
+];
+
+// Questions
+var q2 = [
+	'What are you working on?',
+	'How does your product work in more detail?',
+	'How do you know customers need what you’re making? How do you know people want this?',
+	'Why isn’t someone already doing this?',
+	'How will you make money?',
+	'How many users are paying?',
+	'How are you understanding customer needs?',
+	'What’s new about what you make?',
+	'How is your product different?',
+	'Where do new users come from? How do users find out about you?',
+	'What makes new users try you?',
+	'How many users do you have?',
+	'How many users are paying?',
+	'How will you make money?',
+	'What has surprised you about user behavior?',
+	'What resistance will users have to trying you and how will you overcome it?'	
+];
+// Questions
+var q3 = [
+	'What are you working on?',
+	'How does your product work in more detail?',
+	'Where is the rocket science here?',
+	'Why did your team get together?',
+	'Who in your team does what?',
+	'What else have you created together?',
+	'Are you open to changing your idea?',
+	'If your startup succeeds, what additional areas might you be able to expand into?',
+	'What is your burn rate?',
+	'How long can you go before funding?',
+	'What is something surprising you have done?',
+	'How big an opportunity is there?',
+	'Who might become competitors?',
+	'What do you understand that others don’t?',
+	'Who is going to be your first paying customer?',
+	'What resistance will users have to trying you and how will you overcome it?'
+];
+
+var q4 = [
+	'What are you working on?',
+	'How do you know customers need what you’re making? How do you know people want this?',
+	'How did your team meet?',
+	'Who would you hire or how would you add to your team? Who would be your next hire?',
+	'Who is “the boss”? ',
+	'What problems and hurdles are you anticipating? How will you overcome them?',
+	'Tell us about a tough problem you solved?',
+	'What’s the worst thing that has happened? What’s the biggest mistake you have made?',
+	'In what ways are you resourceful?',
+	'If your startup succeeds, what additional areas might you be able to expand into?',
+	'Someone just showed us an idea like this right before you guys. I don’t like it. What else do you have?',
+	'Have you raised funding?',
+	'What is your burn rate?',
+	'How long can you go before funding?',
+	'Are you open to changing your idea?',
+	'How do we know your team will stick together? Will your team stick at this?'
+];
+
+var q5 = [
+	'What are you working on?',
+	'Why isn’t someone already doing this?',
+	'Why will you succeed over others? ',
+	'How did your team meet?',
+	'Who in your team does what?',
+	'What else have you created together?',
+	'What is your distribution strategy? How will you grow?',
+	'What problems and hurdles are you anticipating? How will you overcome them?',
+	'Six months from now, what’s going to be your biggest problem?',
+	'What’s the funniest thing that has happened to you?',
+	'Where do new users come from? How do users find out about you?',
+	'What makes new users try you?',
+	'Who is going to be your first paying customer?',
+	'Why did you choose this idea? Why did you pick this idea to work on?',
+	'Are you open to changing your idea?',
+	'If your startup succeeds, what additional areas might you be able to expand into?'
+];
+
+var q6 = [
+	'What are you working on?',
+	'Who are your competitors?',
+	'What, exactly, makes you different from existing options?',
+	'Have you contacted any companies who are willing to use?',
+	'How big is your team?',
+	'Who would you hire or how would you add to your team? ',
+	'What is something surprising you have done?',
+	'What’s the worst thing that has happened? What’s the biggest mistake you have made?',
+	'Is anyone using this?',
+	'How much they are paying?',
+	'What has surprised you about user behavior?',
+	'How much money could you make per year?',
+	'Who might become competitors?',
+	'What do you understand that others don’t?',
+	'Where is the rocket science here?',
+	'What will you do if we don’t fund you?'
+];
+
+var q7 = [
+	'What are you making and how does it work?',
+	'Who does what in the team?',
+	'How did your team meet?',
+	'Who would be paying for it?',
+	'What else have you created together?',
+	'Are you open to changing your idea?',
+	'How are you meeting customers?',
+	'What makes new users try you?',
+	'What resistance will users have to trying you and how will you overcome it?',
+	'Six months from now, what’s going to be your biggest problem?',
+	'What are the key things about your field that outsiders don’t understand?',
+	'What domain expertise do you have?',
+	'What are you going to do next?',
+	'What else have you created together?',
+	'How do we know your team will stick together? Will your team stick at this?',
+	'Are you open to changing your idea?'
+];
+
+var q8 = [
+	'What are you working on?',
+	'Do you have a demo?',
+	'What domain expertise do you have?',
+	'How big an opportunity is there?',
+	'Why isn’t someone already doing this?',
+	'Why will you succeed over others? ',
+	'Where is the rocket science here?',
+	'What are you going to do next?',
+	'What is your distribution strategy? How will you grow?',
+	'What is your burn rate?',
+	'How long can you go before funding?',
+	'Six months from now, what’s going to be your biggest problem?',
+	'How much money could you make per year?',
+	'How much does customer acquisition cost?',
+	'How are you different from competitors',
+	'What do you understand that others don’t?'
+];
+// Tips
+var tips = [
+	'4Try to have quick and concise answers.',
+	'Try to answer in 2 to 3 sentences.',
+	'Answer like you would in a conversation, this is not a presentation.',
+	'When talking to PG and the other YC partners, be substantive, and get straight to the point.',
+	'DO NOT BEAT AROUND THE BUSH. Do not hem and haw. "Uhm"s and "uh"s don&#39;t lend an aura of intelligence.',
+	'Use precise and concise language. You aren&#39;t pitching a VC that will ask you what your "secret sauce" is.',
+	'He talks quickly, and so do the other YC partners. Try to match their tempo (as you should when talking to anyone).'
+];
---
+++ functions.js
@@ -0,0 +1,117 @@
+/* --- Question Functions ---
+   -------------------------------------------------- */
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return results[1] || 0;
+    }
+}
+
+function next_q()
+{
+	if ( $('#end').hasClass('hide') == true )
+	{
+		//alert("change");
+		var question = q.shift();
+		
+		QuestionTime[counter] = new Date();
+		AnswerTime[counter] = (QuestionTime[counter] - QuestionTime[counter-1])/1000;
+		
+		if (question != 'done')
+		{
+			QuestionList[counter] = question;
+    		
+    		var qs = $.urlParam('question');  
+			if(qs != null) 
+    		{
+				$('#question').html(question);
+    		}
+			var msg = new SpeechSynthesisUtterance(question);
+			window.speechSynthesis.speak(msg);
+			
+			timer_restart();
+			display_tip();
+		} else
+		{
+			$('#question').addClass('hide');
+			$('#timer').addClass('hide');
+			$('.pg').addClass('hide');
+			
+			$('#end').removeClass('hide');
+			$('#resulttable').addClass('resultclass');
+			var totalTime = 0;
+			for (var i = 0; i < counter; i++) {
+				$('#resulttable').append("<tr><td>" + QuestionList[i] + "</td><td>" + AnswerTime[i+1] + "</td></tr>"); 
+				totalTime = totalTime + AnswerTime[i+1];
+			}
+			$('#resulttable').append("<tr><td><b>Total</b></td><td><b>" + totalTime + "</b></td></tr>"); 
+				
+			
+		}
+		//var tip = tips.shift();
+//		$('#tip').html(AnswerTime[counter] + list[counter]);
+		counter++;
+	}
+}
+
+
+
+/* --- Timer Functions ---
+   -------------------------------------------------- */
+
+function timer_tick()
+{
+	$('#timer').removeClass('warning');
+	$('#timer').removeClass('fail');
+	
+	timer = timer - 1;
+	
+	$('#timer').html(timer + ' s');
+	
+	
+	if (timer < 10)
+	{
+		$('#timer').addClass('warning');
+	}
+	if (timer < 5)
+	{
+		$('#timer').removeClass('warning');
+		$('#timer').addClass('fail');
+	}
+	if (timer <= 0)
+	{
+		$('#timer').addClass('hide');
+		//$('.pg').removeClass('hide');
+	}
+	
+	 setTimeout('timer_tick();', 1000);
+}
+
+function timer_restart()
+{
+	$('#timer').removeClass('warning');
+	$('#timer').removeClass('fail');
+	$('.pg').addClass('hide');
+	$('#timer').removeClass('hide');
+	
+	timer = 30;
+	
+	$('#timer').html('30 s');
+}
+
+
+
+/* --- Tip Functions ---
+   -------------------------------------------------- */
+
+function display_tip()
+{
+	var tip = tips.shift();
+	
+	$('#tip').html(tip);
+	
+	tips.push(tip);
+}
